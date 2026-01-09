927d50cea3a8f0f27d24f9e63cf1176440245ee8
mayboll <igonzalezs@ceinmark.net>
2026-01-09 08:42:57 +0100

    Feat: added new nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,217 +1,106 @@
--- Option configuration
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.cursorline = true
-vim.opt.shiftwidth = 4
-vim.opt.termguicolors = true
-vim.opt.tabstop = 4
-vim.opt.expandtab = true
-vim.opt.scrolloff = 999
-vim.opt.winborder = "rounded"
-vim.opt.smartindent = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.undofile = true
-vim.opt.signcolumn = "number"
-vim.opt.guicursor = "n-v-i-c:block-Cursor"
+-- Options
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.smartindent = true
+vim.o.winborder = "rounded"
+vim.o.cursorline = true
+vim.o.scrolloff = 999
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.signcolumn = "yes"
+vim.o.guicursor = "n-v-i-c:block-Cursor"
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.diagnostic.config({ virtual_text = true })
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
--- Netrw customization
-vim.g.netrw_banner = 0
-vim.g.netrw_liststyle = 3
-
--- Plugins manager
+-- Package management
 vim.pack.add({
-    { src = "https://github.com/windwp/nvim-autopairs" },
+	{ src = "https://github.com/windwp/nvim-autopairs" },
     { src = "https://github.com/AlphaTechnolog/pywal.nvim" },
-    { src = "https://github.com/nvim-lualine/lualine.nvim" },
-    { src = "https://github.com/nvim-telescope/telescope.nvim" },
-    { src = "https://github.com/nvim-lua/plenary.nvim" },
-    { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
-    { src = "https://github.com/brenoprata10/nvim-highlight-colors" },
+	{ src = "https://github.com/nvim-tree/nvim-tree.lua" },
+	{ src = "https://github.com/brenoprata10/nvim-highlight-colors" },
+	{ src = "https://github.com/mason-org/mason.nvim" },
+	{ src = "https://github.com/mini.nvim/mini.pick" },
+	{ src = "https://github.com/L3MON4D3/LuaSnip" },
+	{ src = "https://github.com/rafamadriz/friendly-snippets" },
     { src = "https://github.com/nvim-tree/nvim-web-devicons" },
-    { src = "https://github.com/L3MON4D3/LuaSnip" },
-    { src = "https://github.com/neovim/nvim-lspconfig" },
-    { src = "https://github.com/mason-org/mason.nvim" },
-    { src = "https://github.com/mason-org/mason-lspconfig.nvim" },
-    { src = "https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim" },
-    { src = "https://github.com/rafamadriz/friendly-snippets" },
-    { src = "https://github.com/saghen/blink.cmp" },
+	{ src = "https://github.com/saghen/blink.cmp" },
+	{ src = "https://github.com/nvim-mini/mini.surround" },
+	{ src = "https://github.com/nvim-mini/mini.comment" },
 })
 
--- Standalone plugis with no config
+-- Plugin setup
+require("nvim-tree").setup()
 require("nvim-autopairs").setup()
-require("nvim-highlight-colors").setup()
+require("nvim-highlight-colors").setup({})
+require("mason").setup()
+vim.lsp.enable("lua_ls")
+require("mini.pick").setup()
 require("luasnip").setup({ enable_autosnippets = true })
 require("luasnip.loaders.from_vscode").lazy_load()
-
--- Telescope config
-local telescope = require("telescope")
-telescope.setup({
-    pickers = {
-        find_files = {
-            hidden = true
-        }
-    }
-})
-
--- Mason config
-require("mason").setup()
-require("mason-lspconfig").setup()
-require("mason-tool-installer").setup({
-    ensure_installed = {
-        "lua_ls",
-        "clangd",
-        "pyright",
-        "jdtls",
-        "html",
-    },
-    auto_install = { enable = true},
-})
-
-vim.lsp.config("lua_ls", {
-    settings = {
-        Lua = {
-            runtime = {
-                version = 'LuaJIT',
-            },
-            diagnostics = {
-                globals = {
-                    'vim',
-                    'require'
-                },
-            },
-            workspace = {
-                library = vim.api.nvim_get_runtime_file("", true),
-            },
-            telemetry = {
-                enable = false,
-            },
-        },
-    },
-})
+require("mini.surround").setup()
+require("mini.comment").setup()
 
 require("blink.cmp").setup({
-    signature = { enabled = true },
-    fuzzy = { implementation = "lua" },
-    completion = {
-        documentation = { auto_show = false },
-        menu = {
-            auto_show = true,
-            draw = {
-                treesitter = { "lsp" },
-                columns = { { "kind_icon", "label", "label_description", gap = 1 }, { "kind" } },
-            },
-        },
-    },
+	signature = { enabled = true },
+	fuzzy = { implementation = "lua" },
+	completion = {
+		documentation = { auto_show = true },
+		menu = {
+			auto_show = true,
+			draw = {
+				treesitter = { "lsp" },
+				columns = { { "kind_icon", "label", "label_description", gap = 1 }, { "kind" } },
+			},
+		},
+	},
 })
 
-vim.diagnostic.config({ virtual_text = true, virtual_lines = false })
 
--- Treesitter
-local treesitter = require("nvim-treesitter")
-treesitter.setup({
-    highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-    },
-    indent = {
-        enable = true,
-    },
-    autotage = { enable = true },
-    ensure_installed = {
-        "lua",
-        "java",
-        "c",
-        "python",
-        "cpp",
-        "css",
-        "html",
-        "php",
-        "markdown",
-    },
-    auto_install = { enable = false },
-})
 
--- Theme and lualine config
-require("pywal").setup()
-vim.api.nvim_set_hl(0, "CursorLine", {
-    bg = "#3c3f41", -- JetBrains Darcula style background for cursor line
-})
-vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#FFCC66", bold = true })
-local fg_colors = {
-    Normal     = "#A9B7C6", -- texto normal
-    Comment    = "#808080", -- comentarios
-    Keyword    = "#CC7832", -- palabras clave
-    Identifier = "#A9B7C6", -- nombres de variables
-    Function   = "#FFC66D", -- funciones
-    Statement  = "#CC7832", -- declaraciones
-    Type       = "#A9B7C6", -- tipos
-    Constant   = "#6897BB", -- constantes
-    String     = "#6A8759", -- cadenas
-    Number     = "#6897BB", -- números
-    Operator   = "#A9B7C6", -- operadores
-}
-
-for group, color in pairs(fg_colors) do
-    vim.api.nvim_set_hl(0, group, { fg = color })
-end
-
-require("lualine").setup {
-    options = {
-        icons_enabled = true,
-        theme = 'auto',
-        component_separators = '',
-        section_separators = { left = '', right = '' },
-        component_separators = { '', '' },
-        disabled_filetypes = {
-            statusline = {},
-            winbar = {},
-        },
-        ignore_focus = {},
-        always_divide_middle = true,
-        always_show_tabline = true,
-        globalstatus = false,
-        refresh = {
-            statusline = 100,
-            tabline = 100,
-            winbar = 100,
-        }
-    },
-    sections = {
-        lualine_a = { 'mode' },
-        lualine_b = { 'branch', 'diagnostics' },
-        lualine_c = { { 'filename', path = 1 } },
-        lualine_x = { 'filetype' },
-        lualine_y = { 'progress' },
-        lualine_z = { 'location' }
-    },
-    inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = {},
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = {}
-    },
-    tabline = {},
-    winbar = {},
-    inactive_winbar = {},
-    extensions = {}
-}
-
--- Keybinds
+-- Keymaps
+local map = vim.keymap
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
+map.set('n', '<leader>o', ':update<CR> :source<CR>')
+map.set('n', '<leader>w', ':write<CR>')
+map.set('n', '<leader>q', ':quit<CR>')
+map.set('n', '<leader>r', ':make<CR>')
+map.set('n', '<leader>lf', vim.lsp.buf.format)
+map.set('n', '<leader>cd', ":NvimTreeToggle<CR>")
+map.set('n', 'ff', MiniPick.builtin.files)
+map.set('n', 'fg', MiniPick.builtin.grep_live)
+map.set('n', 'fh', MiniPick.builtin.help)
+map.set('n', 'grd', vim.lsp.buf.definition)
+-- grr (go to lsp references)
+-- grn (lsp rename)
+-- grt (go to type definition)
+-- Copy paste shortcuts
+map.set({ 'n', 'v', 'x' }, '<M-y>', '"+y<CR>')
+map.set({ 'n', 'v', 'x' }, '<M-d>', '"+d<CR>')
+map.set({ 'n', 'v', 'x' }, '<M-p>', '"+p<CR>')
+map.set({ 'i', 'c' }, '<M-p>', '<C-r>+')
+map.set("n", "<leader>i", function()
+	vim.cmd.new()
+	vim.cmd.term()
+	vim.cmd.wincmd("J")
+	vim.api.nvim_win_set_height(0, 10)
+end)
+map.set({"t", "n"}, "<Esc>", [[<C-\><C-n>]])
 
-local colors = {}
-local ls = require("luasnip")
-local builtin = require("telescope.builtin")
-local map = vim.keymap.set
-local current = 1
+-- Navigate windows
+map.set("n", "<M-h>", "<C-w>h")
+map.set("n", "<M-j>", "<C-w>j")
+map.set("n", "<M-k>", "<C-w>k")
+map.set("n", "<M-l>", "<C-w>l")
+map.set("n", "<leader>sv", ":vsplit<CR>")
+map.set("n", "<leader>sh", ":split<CR>")
 
-map("n", "<leader>lf", vim.lsp.buf.format)
-map("n", "<leader>gr", "<cmd>Telescope lsp_references<CR>")
-map("n", "<leader>gd", "<cmd>Telescope lsp_definitions<CR>")
-map("n", "<leader>ff", builtin.find_files)
-
-
+-- Resize windows
+map.set("n", "<M-Up>", ":resize +1<CR>")
+map.set("n", "<M-Down>", ":resize -1<CR>")
+map.set("n", "<M-Left>", ":vertical resize -1<CR>")
+map.set("n", "<M-Right>", ":vertical resize +1<CR>")
